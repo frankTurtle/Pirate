31553d71df88a030893ccaeb4b5d45a351c2e9a3
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-18 16:38:00 -0400

    Created Tile class Testing remote push to Github

---
+++ Pirate Assignment/Tile.h
@@ -0,0 +1,13 @@
+//
+//  Tile.h
+//  Pirate Assignment
+//
+//  Created by OSX on 5/18/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Tile : NSObject
+
+@end
---
+++ Pirate Assignment/Tile.m
@@ -0,0 +1,13 @@
+//
+//  Tile.m
+//  Pirate Assignment
+//
+//  Created by OSX on 5/18/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import "Tile.h"
+
+@implementation Tile
+
+@end
