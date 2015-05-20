2ca000bcdcc3e409dda2ccf8f4222f4141bb3ea8
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-20 16:29:39 -0400

    Created armor class with health and name as attributes

---
+++ Pirate Assignment/Armor.h
@@ -0,0 +1,16 @@
+//
+//  Armor.h
+//  Pirate Assignment
+//
+//  Created by OSX on 5/20/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Armor : NSObject
+
+@property int health; //.................................. value to hold the health value
+@property (strong, nonatomic) NSString *armorName; //..... String to hold the name of the armor
+
+@end
---
+++ Pirate Assignment/Armor.m
@@ -0,0 +1,14 @@
+//
+//  Armor.m
+//  Pirate Assignment
+//
+//  Created by OSX on 5/20/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import "Armor.h"
+
+@implementation Armor
+
+
+@end
