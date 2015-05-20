b7724e5eb8556bff893956a0e8094ecd8b49b481
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-20 17:27:26 -0400

    created character class gave it health, weapon, armor attributes and appropriate methods sot set those

---
+++ Pirate Assignment/Character.h
@@ -0,0 +1,26 @@
+//
+//  Character.h
+//  Pirate Assignment
+//
+//  Created by OSX on 5/20/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import "Weapon.h" //............... needed for weapons
+#import "Armor.h" //................ needed for armor
+
+@interface Character : NSObject
+
+// Instance variables
+@property int health; //............................ holds characters health
+@property (strong, nonatomic) Weapon *weapon; //.... holds the characters weapon
+@property (strong, nonatomic) Armor *armor; //...... holds the characters armor
+
+// Methods
+-(void)setHealth:(int)health; //.................... sets the health of the characters
+-(void)setWeapon:(Weapon *)weapon; //............... sets the weapon for the character
+-(void)setArmor:(Armor *)armor; //.................. sets the armor for the character
+
+
+@end
---
+++ Pirate Assignment/Character.m
@@ -0,0 +1,28 @@
+//
+//  Character.m
+//  Pirate Assignment
+//
+//  Created by OSX on 5/20/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import "Character.h"
+
+@implementation Character
+
+-(void)setHealth:(int)health
+{
+    
+}
+
+-(void)setWeapon:(Weapon *)weapon
+{
+    
+}
+
+-(void)setArmor:(Armor *)armor
+{
+    
+}
+
+@end
