230b281413e1b8ef669c94ebfbb8fd5c1ec38840
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-20 18:02:58 -0400

    added comments to characterFactory

--- Pirate Assignment/CharacterFactory.m
+++ Pirate Assignment/CharacterFactory.m
@@ -7,28 +7,28 @@
 //
 
 #import "CharacterFactory.h"
-#import "Character.h"
+#import "Character.h" //....... needed to get a character object
 
 @implementation CharacterFactory
 
 -(Character *)createCharacter
 {
-    Character *character = [[Character alloc] init];
-    Armor *armor = [[Armor alloc] init];
-    Weapon *weapon = [[Weapon alloc] init];
+    Character *character = [[Character alloc] init]; //......... creates the character object to return
+    Armor *armor = [[Armor alloc] init]; //..................... creates an armor object
+    Weapon *weapon = [[Weapon alloc] init]; //.................. creates a weapon object
     
-    [armor setHealth:10];
-    [armor setArmorName:@"Plain Shirt"];
+    [armor setHealth:10]; //.................................... gives the value 10 to the armor
+    [armor setArmorName:@"Plain Shirt"]; //..................... names the armor plain shirt
     
-    [weapon setDamage:1];
-    [weapon setWeaponName:@"Fist"];
+    [weapon setDamage:1]; //.................................... sets the weapon damage to 1
+    [weapon setWeaponName:@"Fist"]; //.......................... names the weapon to fist
     
-    [character setHealth:10];
-    [character setArmor: armor];
-    [character setWeapon:weapon];
-    [character setDamage: (1 + weapon.damage) ];
+    [character setHealth:10]; //............................... sets initial healh to 10
+    [character setArmor: armor]; //............................ sets the initial armor to the one created above
+    [character setWeapon:weapon]; //........................... sets teh weapon to the one created above
+    [character setDamage: (1 + weapon.damage) ]; //............ sets the person's dmg to 1 + weapon dmg
     
-    return character;
+    return character; //....................................... returns the character object
 }
 
 @end
