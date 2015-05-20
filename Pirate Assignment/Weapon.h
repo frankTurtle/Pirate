2474317831b6c015eeacb3524bcef366f3857eb5
Barret J. Nobel <bear.nobel@gmail.com>
2015-05-20 17:13:53 -0400

    Created weapon branch

---
+++ Pirate Assignment/Weapon.h
@@ -0,0 +1,13 @@
+//
+//  Weapon.h
+//  Pirate Assignment
+//
+//  Created by OSX on 5/20/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Weapon : NSObject
+
+@end
---
+++ Pirate Assignment/Weapon.m
@@ -0,0 +1,13 @@
+//
+//  Weapon.m
+//  Pirate Assignment
+//
+//  Created by OSX on 5/20/15.
+//  Copyright (c) 2015 LebonTech solutions. All rights reserved.
+//
+
+#import "Weapon.h"
+
+@implementation Weapon
+
+@end
